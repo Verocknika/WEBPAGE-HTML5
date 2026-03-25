5b13f17d0af4dc360efbbcf65fe120645e9acc80
v_michalkova@utb.cz <v_michalkova@utb.cz>
2026-03-25 12:38:42 +0100

    refactor: global code optimization and HTML syntax fixes

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,3 @@
-// Toggle hamburger menu
 function toggleMenu() {
     document.querySelector('.nav-links').classList.toggle('active');
 }
-
-document.addEventListener('DOMContentLoaded', function () {
-    const mobileMenu = document.getElementById('mobile-menu');
-    if (mobileMenu) {
-        mobileMenu.addEventListener('click', function () {
-            document.querySelector('.nav-links').classList.toggle('active');
-        });
-    }
-});
